2202b466d10ba24ae3a4860ecb57af6166a13f75
BDR159 <60425968+BDR159@users.noreply.github.com>
2025-04-08 20:48:32 +1000

    ECR

--- main.tf
+++ main.tf
@@ -1,61 +1,41 @@
 terraform {
   required_providers {
-      aws = {
+    aws = {
       source = "hashicorp/aws"
       version = "~> 5.0"
     }
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
   }
 }
-  
 provider "aws" {
-region = "us-east-1"
-shared_credentials_files = ["./credentials"]
+  region = "us-east-1"
+  shared_credentials_files = ["./credentials"]
 }
 
-locals {
-  database_username = "administrator"
-  database_password = "foobarbaz" # This is bad!
-}
+data "aws_ecr_authorization_token" "ecr_token" {}
 
-resource "aws_security_group" "taskoverflow_database" {
-  name = "taskoverflow_database"
-  description = "Allow inbound Postgresql traffic"
-  
-  ingress {
-    from_port = 5432
-    to_port = 5432
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "taskoverflow_database"
+provider "docker" {
+  registry_auth {
+    address = data.aws_ecr_authorization_token.ecr_token.proxy_endpoint
+    username = data.aws_ecr_authorization_token.ecr_token.user_name
+    password = data.aws_ecr_authorization_token.ecr_token.password
   }
 }
 
-resource "aws_db_instance" "taskoverflow_database" {
-  allocated_storage = 20
-  max_allocated_storage = 1000
-  engine = "postgres"
-  engine_version = "17"
-  instance_class = "db.t3.micro"
-  db_name = "todo"
-  username = local.database_username
-  password = local.database_password
-  parameter_group_name = "default.postgres17"
-  skip_final_snapshot = true
-  vpc_security_group_ids = [aws_security_group.taskoverflow_database.id]
-  publicly_accessible = true
+resource "aws_ecr_repository" "taskoverflow" {
+  name = "taskoverflow"
+}
 
-  tags = {
-    Name = "taskoverflow_database"
+resource "docker_image" "taskoverflow" {
+  name = "${aws_ecr_repository.taskoverflow.repository_url}:latest"
+  build {
+    context = "."
   }
+}
+
+resource "docker_registry_image" "taskoverflow" {
+  name = docker_image.taskoverflow.name
 }
